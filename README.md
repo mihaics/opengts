a985997b5f4767c3193a71261eed125b8d38c183
Mihai Csaky <mihai.csaky@gmail.com>
2016-05-18 09:17:39 +0300

    cleanup

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Documentation:
+# OpenGTS image based on phusion baseimage
 
-https://github.com/mihaics/opengts-cloud/blob/master/opengtswithkubernetesoncentosatomic.pdf
+docker-compose.yml file is included in git repository as example to run the application
 
--- my_config.sh
+++ my_config.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 #create config from jinja templates
-# environment COMPILE_DEBUG=false, COMPILE_OPTIMIZE=true
-# get environment 
-#export MYSQL_DBNAME=$(curl -L http://$ETCD_SRV_ADDR:2379/v2/keys/opengts/$OPENGTS_CLIENT_ID/MYSQL_DBNAME | jq '.node.value' | sed 's/\"//g'  )
-#export MYSQL_DBUSER=$(curl -L http://$ETCD_SRV_ADDR:2379/v2/keys/opengts/$OPENGTS_CLIENT_ID/MYSQL_DBUSER | jq '.node.value' | sed 's/\"//g'  )
-#export MYSQL_DBPASSWORD=$(curl -L http://$ETCD_SRV_ADDR:2379/v2/keys/opengts/$OPENGTS_CLIENT_ID/MYSQL_DBPASSWORD | jq '.node.value' | sed 's/\"//g'  )
-#export SYSADMIN_PASSWORD=$(curl -L http://$ETCD_SRV_ADDR:2379/v2/keys/opengts/$OPENGTS_CLIENT_ID/SYSADMIN_PASSWORD | jq '.node.value' | sed 's/\"//g'  )
-#export MYSQL_ENV_MYSQL_ROOT_PASSWORD=$(curl -L http://$ETCD_SRV_ADDR:2379/v2/keys/opengts/database/mysql/MYSQL_ROOT_PASSWORD | jq '.node.value' | sed 's/\"//g'  )
-#export CREATE_DATABASE=$(curl -L http://$ETCD_SRV_ADDR:2379/v2/keys/opengts/$OPENGTS_CLIENT_ID/CREATE_DATABASE | jq '.node.value' | sed 's/\"//g'  )
-
 
 j2  $GTS_HOME/build.properties.j2 > $GTS_HOME/build.properties
 j2  $GTS_HOME/config.conf.j2 > $GTS_HOME/config.conf
-
 
 cd $GTS_HOME; ant all
 cp $GTS_HOME/build/*.war $CATALINA_HOME/webapps/
